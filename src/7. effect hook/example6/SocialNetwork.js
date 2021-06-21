01f0ba5cc39a7d168e34535416c4becc48d463d5
Poplica <tu9489@gmail.com>
2021-06-21 11:43:59 -0400

    +

--- src/7. effect hook/example6/SocialNetwork.js
+++ src/7. effect hook/example6/SocialNetwork.js
@@ -3,33 +3,66 @@ import { get } from './mockBackend/fetch';
 
 export default function SocialNetwork() {
   const [data, setData] = useState(null);
-
+  
+  const [menu, setMenu] = useState(null);
   useEffect(() => {
-    Promise.all([get('/menu'), get('/news-feed'), get('/friends')]).then(
-      ([menuResponse, newsFeedResponse, friendsResponse]) => {
-        setData({
-          menu: menuResponse.data,
-          newsFeed: newsFeedResponse.data,
-          friends: friendsResponse.data
-        });
-      }
-    );
-  }, []);
+    get('/menu').then((response) => {
+      setMenu(response.data);
+    })
+  }, [])
+
+  const [newsFeed, setNewsFeed] = useState(null);
+  useEffect(() => {
+    get('/news-feed').then((response) => {
+      setNewsFeed(response.data);
+    })
+  }, [])
+
+  const [friends, setFriends] = useState(null);
+  useEffect(() => {
+    get('/friends').then((response) => {
+      setFriends(response.data);
+    })
+  }, [])
+
+  // useEffect(() => {
+  //   get('/menu').then((response) => {
+  //     setMenu(response.data);
+  //   })
+  //   get('/news-feed').then((response) => {
+  //     setNewsFeed(newsFeedResponse.data);
+  //   })
+  //   get('/friends').then((response) => {
+  //     setFriends(friendsResponse.data);
+  //   })
+  //   // Promise.all([get('/menu'), get('/news-feed'), get('/friends')]).then(
+  //   //   ([menuResponse, newsFeedResponse, friendsResponse]) => {
+  //   //     setMenu(menuResponse.data);
+  //   //     setNewsFeed(newsFeedResponse.data);
+  //   //     setFriends(friendsResponse.data);
+  //   //     // setData({
+  //   //     //   menu: menuResponse.data,
+  //   //     //   newsFeed: newsFeedResponse.data,
+  //   //     //   friends: friendsResponse.data
+  //   //     // });
+  //   //   }
+  //   // );
+  // }, []);
 
   return (
     <div className='App'>
       <h1>My Network</h1>
-      {!data || !data.menu ? <p>Loading..</p> : (
+      {!menu ? <p>Loading..</p> : (
         <nav>
-          {data.menu.map((menuItem) => (
+          {menu.map((menuItem) => (
             <button key={menuItem}>{menuItem}</button>
           ))}
         </nav>
       )}
       <div className='content'>
-        {!data || !data.newsFeed ? <p>Loading..</p> : (
+        {!newsFeed ? <p>Loading..</p> : (
           <section>
-            {data.newsFeed.map(({ id, title, message, imgSrc }) => (
+            {newsFeed.map(({ id, title, message, imgSrc }) => (
               <article key={id}>
                 <h3>{title}</h3>
                 <p>{message}</p>
@@ -38,10 +71,10 @@ export default function SocialNetwork() {
             ))}
           </section>
         )}
-        {!data || !data.friends ? <p>Loading..</p> : (
+        {!friends ? <p>Loading..</p> : (
           <aside>
             <ul>
-              {data.friends
+              {friends
                 .sort((a, b) => (a.isOnline && !b.isOnline ? -1 : 0))
                 .map(({ id, name, isOnline }) => (
                   <li key={id} className={isOnline ? 'online' : 'offline'}>
